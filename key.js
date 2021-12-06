9af1a3358855cf47c0c03f57ceb4ebba56cc733b
aparus <rustamapaev@gmail.com>
2021-12-06 14:16:00 +0300

    duplicate active key with absolute position

--- key.js
+++ key.js
@@ -1,10 +1,18 @@
 const template = `
-<div :class="['key', content.slot1, content.name, activeKeyStyle]" @click="click()">
+
+<div :class="['key', content.slot1, content.name]" @click="click()">
+	<div v-if="activeKeyStyle" :class="['key', content.slot1, content.name, activeKeyStyle]">
+		<div class="slot1">{{content.slot1}}</div>
+		<div class="slot2">{{content.slot2}}</div>
+		<div class="slot3">{{content.slot3}}</div>
+		<div class="slot4">{{content.slot4}}</div>
+	</div>
 	<div class="slot1">{{content.slot1}}</div>
 	<div class="slot2">{{content.slot2}}</div>
 	<div class="slot3">{{content.slot3}}</div>
 	<div class="slot4">{{content.slot4}}</div>
 </div>
+
 `
 
 export default {
